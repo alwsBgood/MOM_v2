a8068a72ba8b9519f3e0ff7a007afbaf4ff4063a
Dima Chebotaru <dchebotaru@emergetms.com>
2020-02-25 20:21:47 +0200

    Clear project

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,23 @@
-var gulp         = require('gulp'),
-		sass         = require('gulp-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		imagemin = require('gulp-imagemin'),
-		pngquant    = require('imagemin-pngquant'),
-		cache       = require('gulp-cache');
-
-gulp.task('img', function() {
-    return gulp.src('app/img/**/*')
-        .pipe(cache(imagemin({
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        })))
-        .pipe(gulp.dest('app/img'));
-});
-
-gulp.task('browser-sync', ['styles', 'scripts'], function() {
-		browserSync.init({
-				server: {
-						baseDir: "./app"
-				},
-				notify: false
-		});
-});
-
-gulp.task('styles', function () {
-	return gulp.src('scss/*.scss')
-	.pipe(sass({
-		includePaths: require('node-bourbon').includePaths
-	}).on('error', sass.logError))
-	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream());
-});
-
-gulp.task('scripts', function() {
-	return gulp.src([
-		'./app/libs/jquery/jquery-2.1.4.min.js',
-		'./app/libs/niftymodal/classie.js',
-    './app/libs/niftymodal/modalEffects.js',
-    './app/libs/masked/mask.js',
-		'./app/libs/plugins-scroll/plugins-scroll.js',
-    './app/libs/slick/slick.min.js'
-		])
-		.pipe(concat('libs.js'))
-		.pipe(gulp.dest('./app/js/'));
-});
-
-gulp.task('watch', function () {
-	gulp.watch('scss/*.scss', ['styles']);
-	gulp.watch('app/libs/**/*.js', ['scripts']);
-	gulp.watch('app/js/*.js').on("change", browserSync.reload);
-	gulp.watch('app/*.html').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['browser-sync', 'watch']);
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass().on('error',sass.logError))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
+}
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: "./app",
+			index: "/index.html"
+		}
+	});
+	gulp.watch('app/scss/**/*.scss', style);
+	gulp.watch('./*.html').on('change',browserSync.reload);
+	gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
